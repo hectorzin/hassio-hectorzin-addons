0b02c999d2d08787765d5a2a9ec48f1795752fd7
Héctor Martínez <103188721+hectorzin@users.noreply.github.com>
2024-10-16 16:19:23 +0200

    Update run.sh

--- hassio-botaffiumeiro-addon/run.sh
+++ hassio-botaffiumeiro-addon/run.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Ruta del archivo YAML persistente
-CONFIG_FILE="/data/config.yaml"
+# Ruta del archivo YAML
+CONFIG_FILE="/botaffiumeiro/data/config.yaml"
 
-# Si no existe el archivo config.yaml, copiar el archivo base
-if [ ! -f "$CONFIG_FILE" ]; then
-    echo "Archivo config.yaml no encontrado en /data, copiando archivo predeterminado..."
+# Actualizando valores en config.yaml
+echo "Actualizando valores en $CONFIG_FILE..."
+
+# Registrar si el archivo existe o no
+if [ -f "$CONFIG_FILE" ]; then
+    echo "El archivo $CONFIG_FILE existe."
+else
+    echo "El archivo $CONFIG_FILE no existe, creando uno nuevo desde el predeterminado."
     cp /botaffiumeiro/config.yaml "$CONFIG_FILE"
 fi
 
-# Actualizando valores en config.yaml desde las opciones del complemento
-echo "Actualizando valores en $CONFIG_FILE..."
+# Mostrar el contenido del archivo YAML antes de las modificaciones
+echo "Contenido del archivo antes de la actualización:"
+cat $CONFIG_FILE
 
 # Telegram settings
 sed -i "s|bot_token:.*|bot_token: \"$BOT_TOKEN\"|" $CONFIG_FILE
@@ -30,7 +36,8 @@ sed -i "s|app_key:.*|app_key: \"$ALIEXPRESS_APP_KEY\"|" $CONFIG_FILE
 sed -i "s|app_secret:.*|app_secret: \"$ALIEXPRESS_APP_SECRET\"|" $CONFIG_FILE
 sed -i "s|tracking_id:.*|tracking_id: \"$ALIEXPRESS_TRACKING_ID\"|" $CONFIG_FILE
 
-echo "Valores actualizados en $CONFIG_FILE:"
+# Mostrar el contenido después de las modificaciones
+echo "Contenido del archivo después de la actualización:"
 cat $CONFIG_FILE
 
 # Iniciar la aplicación
