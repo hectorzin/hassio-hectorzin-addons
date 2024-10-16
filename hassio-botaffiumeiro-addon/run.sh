d40b04b32034337d3bc762a6cdf5880efd3c0e62
Héctor Martínez <103188721+hectorzin@users.noreply.github.com>
2024-10-16 17:07:34 +0200

    Update run.sh

--- hassio-botaffiumeiro-addon/run.sh
+++ hassio-botaffiumeiro-addon/run.sh
@@ -1,45 +1,11 @@
 #!/bin/bash
+set -e
 
-# Ruta del archivo YAML
-CONFIG_FILE="/botaffiumeiro/data/config.yaml"
+# Variables configurables, asignadas desde el config.yaml
+HOST_DATA_PATH=$(bashio::config 'path_to_host_data')
 
-# Actualizando valores en config.yaml
-echo "Actualizando valores en $CONFIG_FILE..."
+# Crear el directorio si no existe
+mkdir -p ${HOST_DATA_PATH}
 
-# Registrar si el archivo existe o no
-if [ -f "$CONFIG_FILE" ]; then
-    echo "El archivo $CONFIG_FILE existe."
-else
-    echo "El archivo $CONFIG_FILE no existe, creando uno nuevo desde el predeterminado."
-    cp /botaffiumeiro/config.yaml "$CONFIG_FILE"
-fi
-
-# Mostrar el contenido del archivo YAML antes de las modificaciones
-echo "Contenido del archivo antes de la actualización:"
-cat $CONFIG_FILE
-
-# Telegram settings
-sed -i "s|bot_token:.*|bot_token: \"$BOT_TOKEN\"|" $CONFIG_FILE
-sed -i "s|delete_messages:.*|delete_messages: $DELETE_MESSAGES|" $CONFIG_FILE
-
-# Amazon settings
-sed -i "s|affiliate_id:.*|affiliate_id: \"$AMAZON_AFFILIATE_ID\"|" $CONFIG_FILE
-
-# Awin settings
-sed -i "s|publisher_id:.*|publisher_id: \"$AWIN_PUBLISHER_ID\"|" $CONFIG_FILE
-
-# Admitad settings
-sed -i "s|publisher_id:.*|publisher_id: \"$ADMITAD_PUBLISHER_ID\"|" $CONFIG_FILE
-
-# AliExpress settings
-sed -i "s|app_key:.*|app_key: \"$ALIEXPRESS_APP_KEY\"|" $CONFIG_FILE
-sed -i "s|app_secret:.*|app_secret: \"$ALIEXPRESS_APP_SECRET\"|" $CONFIG_FILE
-sed -i "s|tracking_id:.*|tracking_id: \"$ALIEXPRESS_TRACKING_ID\"|" $CONFIG_FILE
-
-# Mostrar el contenido después de las modificaciones
-echo "Contenido del archivo después de la actualización:"
-cat $CONFIG_FILE
-
-# Iniciar la aplicación
-echo "Iniciando la aplicación..."
-python3 /botaffiumeiro/botaffiumeiro.py
+# Ejecutar el bot, con la configuración especificada
+python3 /app/botaffiumeiro.py --data ${HOST_DATA_PATH}
